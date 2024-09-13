1957c372d5f6b57e02d57b667d1f20f4d84aed18
Marcello Silvestri <silvestriweb@gmail.com>
2024-09-13 19:36:47 +0100

    Change footer padding and styling

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="py-8 px-12 text-slate-500 dark:text-slate-400 text-center text-xs">
-      2024 — Marcello Silvestri. All rights reserved.
+    <footer className="p-8 text-slate-500 dark:text-slate-400 text-center text-xs">
+      <span className="font-semibold">2024 — Marcello Silvestri. All rights reserved.</span>
       <br />
       Silvestri&reg; and Silvestri Consulting&reg; are registered trademarks of Silvestri Consulting Ltd.
     </footer>
