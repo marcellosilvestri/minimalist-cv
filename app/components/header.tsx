892e3a9f28713e0fec445fda91cef2969ed15a66
Marcello Silvestri <silvestriweb@gmail.com>
2024-09-13 19:36:57 +0100

    Replace Link with CustomLink component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import CustomLink from "./CustomLink";
 import { ArrowLeftIcon } from "@radix-ui/react-icons";
 import { ThemeToggle } from "../components/themeToggle";
 import { usePathname } from "next/navigation";
@@ -8,9 +8,9 @@ export default function Header() {
     <header className="">
       {usePathname() !== "/" && (
         <div className="fixed top-4 left-4">
-          <Link href="/">
+          <CustomLink href="/">
             <ArrowLeftIcon className="h-[1.2rem] w-[1.2rem] text-slate-500 dark:text-white" />
-          </Link>
+          </CustomLink>
         </div>
       )}
       <div className="fixed top-4 right-4">
