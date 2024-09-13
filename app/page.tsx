759207e52262ee6f9c9c4a0a0a39d86cc4791656
Marcello Silvestri <silvestriweb@gmail.com>
2024-09-13 21:41:33 +0100

    Add flex-grow, remove socials

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="flex flex-col justify-between pt-0 md:pt-8 p-8 min-h-screen">
+      <main className="flex flex-col flex-grow justify-between pt-0 md:pt-8 p-8">
         <motion.div initial={{ opacity: 0, x: -30 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.3, ease: "easeOut" }}>
-          <div className="max-w-[60ch] mx-auto w-full space-y-8 py-12">
+          <div className="max-w-[60ch] mx-auto w-full space-y-8 pt-12">
             <Image src="/avatar.webp" width={100} height={100} className="rounded-xl" alt="A photo of Marcello Silvestri" />
             <div className="space-y-6">
               <div className="space-y-3" id="about-me">
@@ -25,28 +25,9 @@ export default function Home() {
               <div className="space-y-2" id="more-about-me">
                 <h2 className="text-lg font-semibold text-slate-700 dark:text-slate-50">More about me</h2>
                 <p className="text-slate-500 dark:text-slate-400">
-                  Read my developer CV{" "}
+                  You can read my developer CV{" "}
                   <CustomLink href="/cv" title="Marcello Silvestri's CV">
                     here
-                  </CustomLink>
-                  .
-                </p>
-                <p className="text-slate-500 dark:text-slate-400">
-                  I'm also on{" "}
-                  <CustomLink href="https://github.com/marcellosilvestri" title="Marcello Silvestri's Github">
-                    Github
-                  </CustomLink>
-                  ,{" "}
-                  <CustomLink href="https://x.com/shirubusutori" title="Marcello Silvestri's Twitter">
-                    Twitter
-                  </CustomLink>
-                  ,{" "}
-                  <CustomLink href="https://www.linkedin.com/in/silvestrimarcello/" title="Marcello Silvestri's LinkedIn">
-                    LinkedIn
-                  </CustomLink>
-                  , and{" "}
-                  <CustomLink href="https://www.instagram.com/" title="Marcello Silvestri's Instagram">
-                    Instagram
                   </CustomLink>
                   .
                 </p>
@@ -57,13 +38,13 @@ export default function Home() {
                   <CustomLink href="https://www.wiredmark.co.uk" title="Wiredmark">
                     Wiredmark
                   </CustomLink>{" "}
-                  is my digital marketing company. Best suited if you want to do SEO or run ads and need a team of creatives who can support you.
+                  is my digital marketing company. Best suited if you want to do SEO, run ads, or need a team of creatives who can support you.
                 </p>
                 <p className="text-slate-500 dark:text-slate-400">
-                  <CustomLink href="https://www.silvestri.consulting" title="Silvestri Consulting">
+                  <CustomLink href="https://www.silvestri.consulting/en/" title="Silvestri Consulting">
                     Silvestri Consulting
                   </CustomLink>{" "}
-                  is my consulting company. This is the way to go if you need my services directly and don't want/need anyone else.
+                  is my consulting company. It's the way to go if you need my services directly and don't want/need anyone else.
                 </p>
               </div>
               <div className="space-y-2" id="other-projects">
