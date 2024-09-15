322d763dea217f5bbdb5b91a8eaeda41d36de23b
Marcello Silvestri <silvestriweb@gmail.com>
2024-09-15 16:57:23 +0100

    Update footer classes

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,7 +4,7 @@ import { Separator } from "@/components/ui/separator";
 
 export default function Footer() {
   return (
-    <footer className="p-8 text-slate-500 dark:text-slate-400 text-center text-xs">
+    <footer className="p-8 max-w-[350px] md:max-w-full mx-auto text-slate-500 dark:text-slate-400 text-center text-xs">
       <div className="flex justify-center gap-4 mb-4">
         <CustomLink href="https://x.com/shirubusutori" hideIcon={true}>
           <TwitterLogoIcon className="w-5 h-5" />
@@ -22,15 +22,18 @@ export default function Footer() {
           <CalendarIcon className="w-5 h-5" />
         </CustomLink>
       </div>
-      <span className="font-semibold">2024 — Marcello Silvestri. All rights reserved.</span>
-      <br />
-      Silvestri&reg; and Silvestri Consulting&reg; are registered trademarks of Silvestri Consulting Ltd.
-      <br />
-      Want a website like this? Check out my{" "}
-      <CustomLink href="https://github.com/marcellosilvestri/minimalist-cv" hideIcon={true}>
-        GitHub repository
-      </CustomLink>
-      .
+      <div>
+        <span className="font-semibold">&copy;2024 — Marcello Silvestri. All rights reserved.</span>
+        <br />
+        Silvestri&reg; and Silvestri Consulting&reg; are registered trademarks of Silvestri Consulting Ltd.
+      </div>
+      <div className="mt-3">
+        Want a website like this? Check out my{" "}
+        <CustomLink href="https://github.com/marcellosilvestri/minimalist-cv" hideIcon={true}>
+          GitHub repository
+        </CustomLink>
+        .
+      </div>
     </footer>
   );
 }
