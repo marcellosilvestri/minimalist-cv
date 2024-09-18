3ce24c36f4aa90ff22001883e373500b6b5ac068
Marcello Silvestri <silvestriweb@gmail.com>
2024-09-18 09:21:22 +0100

    Set homepage priority to 1 in sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,4 +5,16 @@ module.exports = {
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   exclude: ["/icon.png"],
+  transform: async (config, path) => {
+    if (path === "/") {
+      return {
+        loc: path,
+        priority: 1,
+      };
+    }
+    return {
+      loc: path,
+      priority: 0.7,
+    };
+  },
 };
