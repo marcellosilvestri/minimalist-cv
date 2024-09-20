78bdc62388da0bf730cf8bcfa18d545bca88d58c
Marcello Silvestri <silvestriweb@gmail.com>
2024-09-20 14:35:38 +0100

    Make menu fullwidth on mobile

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -14,9 +14,9 @@ export default function Header() {
   const isActive = (path: string) => pathname === path;
 
   return (
-    <header className="fixed top-0 left-0 right-0 bg-white dark:bg-slate-900 flex items-center justify-between px-8 py-4 z-10">
+    <header className="fixed top-0 left-0 right-0 bg-white dark:bg-slate-900 flex items-center justify-between px-8 py-4 z-10 border-b">
       <Link href="/">
-        <Image src="/logo.svg" alt="Logo" width={130} height={130} className="md:w-[130px] w-[100px] dark:brightness-0 dark:invert" />
+        <Image src="/logo.svg" alt="Logo" width={130} height={130} className="md:w-[130px] w-[90px] dark:brightness-0 dark:invert" />
       </Link>
 
       <div className="flex items-center space-x-4">
@@ -65,19 +65,21 @@ export default function Header() {
               <HamburgerMenuIcon className="h-6 w-6 text-gray-600 dark:text-gray-300" />
             </button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" className="bg-slate-50 dark:bg-slate-800 shadow-none">
-            <DropdownMenuItem asChild>
-              <Link href="/" className="w-full">
+          <DropdownMenuContent
+            align="end"
+            className="w-screen rounded-none space-y-2 px-8 py-2 border-b border-r-0 border-l-0 border-t-0 bg-white dark:bg-slate-900 shadow-none">
+            <DropdownMenuItem asChild className="focus:bg-transparent hover:bg-transparent pl-0">
+              <Link href="/" className={`w-full ${isActive("/") ? "font-bold" : ""}`}>
                 Home
               </Link>
             </DropdownMenuItem>
-            <DropdownMenuItem asChild>
-              <Link href="/cv" className="w-full">
+            <DropdownMenuItem asChild className="focus:bg-transparent hover:bg-transparent pl-0">
+              <Link href="/cv" className={`w-full ${isActive("/cv") ? "font-bold" : ""}`}>
                 CV
               </Link>
             </DropdownMenuItem>
-            <DropdownMenuItem asChild>
-              <Link href="/principles" className="w-full">
+            <DropdownMenuItem asChild className="focus:bg-transparent hover:bg-transparent pl-0">
+              <Link href="/principles" className={`w-full ${isActive("/principles") ? "font-bold" : ""}`}>
                 Principles
               </Link>
             </DropdownMenuItem>
